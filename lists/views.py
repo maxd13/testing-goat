c20d1715127a1e252a28dce83741c470a159fc75
Luiz Carlos R Viana <maxd13@devuapple>
2017-10-08 10:01:17 -0500

    Added a URL for creating a new list via POST. Added a new test class for this and refactored two older tests, moving them to the new class.

--- lists/views.py
+++ lists/views.py
@@ -4,18 +4,15 @@ from .models import Item
 
 
 def home_page(request):
-    if request.method == 'POST':
-        return home_page_POST(request)
     return render(request, 'lists/home_page.html')
 
-
-def home_page_POST(request):
-    item = Item()
-    item.text = request.POST.get('item_text', '')
-    if item.text:
-        item.save()
+def new_list(request):
+    if request.method != 'POST': 
+        return redirect('/')
+    Item.objects.create(text=request.POST['item_text'])
     return redirect('/lists/the-only-list-in-the-world/')
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'lists/list.html', {'items': items})
+
