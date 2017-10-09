6e8def308fef9f95133a2e857a4f8bcef0b2b6de
Luiz Carlos R Viana <maxd13@devuapple>
2017-10-09 15:03:07 -0500

    Refactored url files so that it is all less repetitive and more DRY. Now superlists/urls.py serves the main page, while urls.py treats ^lists/ regex.

--- superlists/urls.py
+++ superlists/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from lists import views as list_views
+
 
 urlpatterns = [
-    url(r'', include('lists.urls')),
+    url(r'^$', list_views.home_page, name='home'),
+    url(r'^lists/', include('lists.urls')),
     url(r'^admin/', admin.site.urls),
 ]
