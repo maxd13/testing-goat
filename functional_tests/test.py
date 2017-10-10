6f7a409ecb8bd541bfc750d381197ab0ae86eea1
Luiz Carlos R Viana <maxd13@devuapple>
2017-10-10 18:37:31 -0500

    Hacked FT runner to be able to test staging

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -12,6 +12,9 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome(driverpath) #"/home/maxd13/Downloads/chromedriver")
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
 
     def tearDown(self):
         self.browser.quit()
